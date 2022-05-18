41525046c8ea8405e4674d0f6e6a375e81e81a1a
yoodongan <dbehddks519@naver.com>
2022-05-18 11:03:06 +0900

    상속관계 매핑 : 싱글테이블 전략, 그냥 전략명만 바꿔주면 끝

--- src/main/java/Item.java
+++ src/main/java/Item.java
@@ -1,7 +1,8 @@
 import javax.persistence.*;
 
 @Entity
-@Inheritance(strategy = InheritanceType.JOINED)
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn
 public class Item {
     @Id @GeneratedValue
     private Long id;
